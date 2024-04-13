d79fc2e790f4e99cd2296edeb64ea41f1430895c
shuklansh <ansh.shukla270501@gmail.com>
2024-04-13 12:17:19 +0530

    QuoteView

---
+++ IOS networking app/QuoteView.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteView.swift
+//  IOS networking app
+//
+//  Created by Ansh Shukla on 13/04/24.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteView()
+}
