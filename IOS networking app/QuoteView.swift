55f5431339fcb15d08576ae6db39831039e774fe
shuklansh <ansh.shukla270501@gmail.com>
2024-04-13 12:17:41 +0530

    QuoteView impl

--- IOS networking app/QuoteView.swift
+++ IOS networking app/QuoteView.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 struct QuoteView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            // used to adapt app to screen size
+            ZStack {
+                Image("breakingbad")
+                    .resizable()
+                    .frame(
+                        width: geo.size.width * 2.7,
+                        height: geo.size.height * 1.2
+                    )
+                VStack {
+                    Text(
+                        "\"ahsdbasbdhasbdhabdashbdashdbashdbahsbdhasbdadbhasbdahsbd\""
+                    )
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .frame( width: geo.size.width)
+                    .padding()
+                }
+            }
+            .frame(
+                width:geo.size.width,
+                height:geo.size.height
+            )
+        }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
     QuoteView()
+        .preferredColorScheme(.dark)
 }
