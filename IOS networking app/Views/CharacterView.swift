8bfdff66b3d7bd06db6a9d6864b7eeb761526589
shuklansh <ansh.shukla270501@gmail.com>
2024-04-15 22:07:25 +0530

    CharacterView with horizontal scroll and sheet impl

---
+++ IOS networking app/Views/CharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterView.swift
+//  IOS networking app
+//
+//  Created by Ansh Shukla on 15/04/24.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterView()
+}
